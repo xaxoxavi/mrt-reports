cf23dfe592caa234d4a2c83463b2451cbbf6350c
Julien <julien.gillet@sembo.se>
2023-04-25 22:15:55 +0200

    controller cleanup

--- src/main/java/com/mistertransfer/reports/ReportController.java
+++ src/main/java/com/mistertransfer/reports/ReportController.java
@@ -1,27 +1,37 @@
 package com.mistertransfer.reports;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.engine.TemplateData;
+
+import java.util.Map;
 
 @RestController
 public class ReportController {
 
-    @PostMapping("/generate")
-    public String generateTemplate(@RequestBody String json) {
-        JsonObject data = JsonParser.parse(json).getAsJsonObject();
-        String title = data.get("title").getAsString();
-        String message = data.get("message").getAsString();
+    @PostMapping("/generate/html")
+    public String generateTemplate(@RequestBody TemplateData templateData) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> model = mapper.convertValue(templateData, new TypeReference<>() {});
 
-        Context context = new Context();
-        context.setVariable("title", title);
-        context.setVariable("message", message);
+        WebContext context = new WebContext(null, null, null);
+        context.setVariables(model);
+        String html = new TemplateEngine().process("receipt-mailEN.html", context);
 
-        String html = new TemplateEngine().process("template", context);
-
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_HTML);
         return html;
     }
 
