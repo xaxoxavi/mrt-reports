c857b438326aa69b32bffee0def0ab62bc50b379
xaxoxavi <xavi.torrens@gmail.com>
2023-05-09 16:12:23 +0200

    lombock example

--- src/main/java/com/mistertransfer/reports/domain/ReceiptData.java
+++ src/main/java/com/mistertransfer/reports/domain/ReceiptData.java
@@ -1,49 +1,20 @@
 package com.mistertransfer.reports.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class ReceiptData {
-    public String getReceiptNumber() {
-        return receiptNumber;
-    }
-
-    public void setReceiptNumber(String receiptNumber) {
-        this.receiptNumber = receiptNumber;
-    }
-
-    public String getDatePaid() {
-        return datePaid;
-    }
-
-    public void setDatePaid(String datePaid) {
-        this.datePaid = datePaid;
-    }
-
-    public String getLocataList() {
-        return locataList;
-    }
-
-    public void setLocataList(String locataList) {
-        this.locataList = locataList;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
 
     private String receiptNumber;
     private String datePaid;
     private String locataList;
     private String amount;
     private String currency;
+
 }
